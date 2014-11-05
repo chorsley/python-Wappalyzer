ce6cf019a96975aad2b4602bbcbbc0e104539991
Chris Horsley <chris.horsley@csirtfoundry.com>
2014-11-06 09:25:06 +1000

    PEP8, fix package_data

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "python-Wappalyzer",
-    version = "0.2.2",
-    description = "Python implementation of the Wappalyzer web application detection utility",
-    author = "Clay McClure",
-    author_email = "clay@daemons.net",
-    url = "https://github.com/claymation/python-Wappalyzer",
-    classifiers = [
+    name="python-Wappalyzer",
+    version="0.2.2",
+    description="Python implementation of the Wappalyzer web application "
+                "detection utility",
+    author="Clay McClure",
+    author_email="clay@daemons.net",
+    url="https://github.com/claymation/python-Wappalyzer",
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
     ],
-    py_modules = ['Wappalyzer'],
     packages=find_packages(),
-    package_data = {'': ['data/apps.json']},
-    install_requires = [
+    package_data={'Wappalyzer': ['data/apps.json']},
+    install_requires=[
         'BeautifulSoup==3.2.1',
         'requests',
     ],
-    test_suite = 'nose.collector',
-    tests_require = [
+    test_suite='nose.collector',
+    tests_require=[
         'httpretty>=0.6.0',
         'nose>=1.3.0',
         'requests'
