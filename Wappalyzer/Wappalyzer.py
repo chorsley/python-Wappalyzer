d14b3175f28448c5e93351f0ae180ff9b5f52da3
atcazzual <atcazzual@users.noreply.github.com>
2014-12-12 16:57:13 -0500

    Added missing Wappalyzer functionality
    Added version grabbing, confidence rating and "env" check (commented out).  The "env" check code is currently commented out pending a decision on whether to make PhantomJS a requirement.

--- Wappalyzer/Wappalyzer.py
+++ Wappalyzer/Wappalyzer.py
@@ -5,6 +5,8 @@ import os
 import logging
 import pkgutil
 import pkg_resources
+# Commenting out 'env' functionality until decision on whether to make PhantomJS a requirement
+#from   selenium import webdriver
 
 import requests
 
@@ -43,13 +45,16 @@ class WebPage(object):
         self.url = url
         self.html = html
         self.headers = headers
-
+        
         try:
             self.headers.keys()
         except AttributeError:
             raise ValueError("Headers must be a dictionary-like object")
 
         self._parse_html()
+        
+        # Commenting out 'env' functionality until decision on whether to make PhantomJS a requirement
+        #self._get_window_attributes()
 
     def _parse_html(self):
         """
@@ -64,6 +69,36 @@ class WebPage(object):
                     'meta', attrs=dict(name=True, content=True))
         }
 
+# Commenting out 'env' functionality until decision on whether to make PhantomJS a requirement
+#        def _get_window_attributes(self):
+#        """
+#        Get window attributes via PhantomJS for the 'env' check
+#        """
+#        # Set Accept-Encoding header to not allow gzip encoding to avoid known issue 
+#        # where gzip encoding *sometimes* causes parse errors
+#        # (https://code.google.com/p/phantomjs/issues/detail?id=930&start=300)
+#        webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.Accept-Encoding'] = 'gzip;q=0,deflate,sdch'
+#        d = webdriver.PhantomJS(service_args=[
+#            '--load-images=false',
+#            '--ignore-ssl-errors=true'
+#            ])
+#        try:
+#            d.get(self.url)
+#        except:
+#            d.quit()
+#            raise
+
+#        script = 'var a = []; for (var key in window) {a.push(key)};return a;'
+#        try:
+#            self.env = d.execute_script(script)
+#        except:
+#            #Some pages fail when executing javascript
+#            print url + ' fails to execute script.'
+#            self.env = []
+#
+#        d.quit()
+
+
     @classmethod
     def new_from_url(cls, url, verify=True):
         """
@@ -76,6 +111,8 @@ class WebPage(object):
         url : str
         verify: bool
         """
+
+        url = url.split('#', 1)[0]
         response = requests.get(url, verify=verify, timeout=2.5)
         return cls.new_from_response(response)
 
@@ -133,8 +170,9 @@ class Wappalyzer(object):
         """
         Normalize app data, preparing it for the detection phase.
         """
-
         # Ensure these keys' values are lists
+        # Commenting out 'env' functionality until decision on whether to make PhantomJS a requirement
+        #for key in ['url', 'html', 'script', 'env', 'implies']:
         for key in ['url', 'html', 'script', 'implies']:
             try:
                 value = app[key]
@@ -149,19 +187,21 @@ class Wappalyzer(object):
             try:
                 value = app[key]
             except KeyError:
-                app[key] = {}
+                app[key] = {}            
 
         # Ensure the 'meta' key is a dict
         obj = app['meta']
         if not isinstance(obj, dict):
-            app['meta'] = {'generator': obj}
+            app['meta'] = { 'generator': obj }
 
         # Ensure keys are lowercase
         for key in ['headers', 'meta']:
             obj = app[key]
-            app[key] = {k.lower(): v for k, v in obj.items()}
+            app[key] = { k.lower(): v for k,v in obj.items() }
 
         # Prepare regular expression patterns
+        # Commenting out 'env' functionality until decision on whether to make PhantomJS a requirement
+        #for key in ['url', 'html', 'script', 'env']:
         for key in ['url', 'html', 'script']:
             app[key] = [self._prepare_pattern(pattern) for pattern in app[key]]
 
@@ -175,49 +215,137 @@ class Wappalyzer(object):
         Strip out key:value pairs from the pattern and compile the regular
         expression.
         """
-        regex, _, rest = pattern.partition('\\;')
-        try:
-            return re.compile(regex, re.I)
-        except re.error as e:
-            warnings.warn(
-                "Caught '{error}' compiling regex: {regex}"
-                .format(error=e, regex=regex)
-            )
-            # regex that never matches:
-            # http://stackoverflow.com/a/1845097/413622
-            return re.compile(r'(?!x)x')
+        attrs = {}
+        pattern = pattern.split('\\;')
+        for idx, expression in enumerate(pattern):
+            if idx == 0:
+                attrs['string'] = expression
+                try:
+                    attrs['regex'] = re.compile(expression, re.I)
+                except re.error as e:
+                    warnings.warn(
+                        "Caught '{error}' compiling regex: {regex}"
+                        .format(error=e, regex=regex)
+                    )
+                    # regex that never matches:
+                    # http://stackoverflow.com/a/1845097/413622
+                    attrs['regex'] = re.compile(r'(?!x)x')
+            else:
+                attr = expression.split(':')
+                if len(attr) > 1:
+                    key = attr.pop(0)
+                    attrs[str(key)] = ':'.join(attr)
+        return attrs
 
-    def _has_app(self, app, webpage):
+    def _has_app(self, app_name, app, webpage):
         """
         Determine whether the web page matches the app signature.
         """
-        # Search the easiest things first and save the full-text search of the
-        # HTML for last
+        hasApp = False
+        # Search the easiest things first and save the full-text search of the HTML for last
+        for pattern in app['url']:
+            if pattern['regex'].search(webpage.url):
+                self._set_detected_app(app_name, app, 'url', pattern, webpage.url)
+                #return True
+                hasApp = True
 
-        for regex in app['url']:
-            if regex.search(webpage.url):
-                return True
-        for name, regex in app['headers'].items():
+        for name, pattern in app['headers'].items():
             if name in webpage.headers:
                 content = webpage.headers[name]
-                if regex.search(content):
-                    return True
+                if pattern['regex'].search(content):
+                    self._set_detected_app(app_name, app, 'headers', pattern, content, name)
+                    #return True
+                    hasApp = True
+                    
+# Commenting out 'env' functionality until decision on whether to make PhantomJS a requirement
+#        for pattern in app['env']:
+#            for property in webpage.env:
+#                if pattern['regex'].search(property):
+#                    self._set_detected_app(app_name, app, 'env', pattern, webpage.env)
+#                    hasApp = True
 
-        for regex in app['script']:
+        for pattern in app['script']:
             for script in webpage.scripts:
-                if regex.search(script):
-                    return True
+                if pattern['regex'].search(script):
+                    self._set_detected_app(app_name, app, 'script', pattern, script)
+                    #return True
+                    hasApp = True
 
-        for name, regex in app['meta'].items():
+        for name, pattern in app['meta'].items():
             if name in webpage.meta:
                 content = webpage.meta[name]
-                if regex.search(content):
-                    return True
+                if pattern['regex'].search(content):
+                    self._set_detected_app(app_name, app, 'meta', pattern, content, name)
+                    #return True
+                    hasApp = True
 
-        for regex in app['html']:
-            if regex.search(webpage.html):
-                return True
+        for pattern in app['html']:
+            if pattern['regex'].search(webpage.html):
+                self._set_detected_app(app_name, app, 'html', pattern, webpage.html)
+                #return True
+                hasApp = True
+        
+        # Set total confidence
+        if hasApp:
+            total = 0
+            for id in app['confidence']:
+                total += app['confidence'][id]
+            app['confidenceTotal'] = total
 
+        return hasApp
+    
+    def _set_detected_app(self, app_name, app, type, pattern, value, key = ''):
+        """
+        Store detected app
+        """
+        app['detected'] = True
+        
+        # Set confidence level
+        if key != '':
+            key += ' '
+        if 'confidence' not in app:
+            app['confidence'] = {}
+        if 'confidence' not in pattern:
+            pattern['confidence'] = 100
+        else:
+            # Convert to int for easy adding later
+            pattern['confidence'] = int(pattern['confidence'])
+        app['confidence'][type + ' ' + key + pattern['string']] = pattern['confidence']
+        
+        # Detect version number
+        if 'version' in pattern:
+            allmatches = re.findall(pattern['regex'], value)
+            for i, matches in enumerate(allmatches):
+                version = pattern['version']
+                # Check for a string to avoid enumerating the string
+                if isinstance(matches, basestring):
+                    matches = [(matches)]
+                for idx, match in enumerate(matches):
+                    # Parse ternary operator
+                    ternary = re.search(re.compile('\\\\' + str(idx+1) + '\\?([^:]+):(.*)$', re.I), version)
+                    if ternary and len(ternary.groups()) == 2 and ternary.group(1) is not None and ternary.group(2) is not None:
+                        version = version.replace(ternary.group(0), ternary.group(1) if match != '' else ternary.group(2))
+                    
+                    # Replace back references
+                    version = version.replace('\\' + str(idx+1), match);
+                if version != '':
+                    if 'versions' not in app:
+                        app['versions'] = [version]
+                    elif version not in app['versions']:
+                        app['versions'].append(version)
+            #self._set_app_version(app)
+        return app_name
+
+    def _set_app_version(self, app):
+        """
+		Resolve version number (find the longest version number that contains
+        all shorter detected version numbers)
+		"""
+        if 'versions' not in app:
+            return
+        
+        app['versions'] = sorted(app['versions'], cmp=_sort_app_versions)
+        
     def _get_implied_apps(self, detected_apps):
         """
         Get the set of apps implied by `detected_apps`.
@@ -250,17 +378,32 @@ class Wappalyzer(object):
                      for cat_num in cat_nums]
 
         return cat_names
+        
+    def get_versions(self, app_name):
+        """
+        Returns a list of the disovered versions for an app name.
+        """
+        return [] if 'versions' not in self.apps[app_name] else self.apps[app_name]['versions']
+
+    def get_confidence(self, app_name):
+        """
+        Returns the total confidence for an app name.
+        """
+        return [] if 'confidenceTotal' not in self.apps[app_name] else self.apps[app_name]['confidenceTotal']
 
     def analyze(self, webpage):
         """
         Return a list of applications that can be detected on the web page.
         """
         detected_apps = set()
+        detected_apps_with_versions = set()
 
+        # Check for app
         for app_name, app in self.apps.items():
-            if self._has_app(app, webpage):
+            if self._has_app(app_name, app, webpage):
                 detected_apps.add(app_name)
 
+        # Add implied apps
         detected_apps |= self._get_implied_apps(detected_apps)
 
         return detected_apps
@@ -274,3 +417,8 @@ class Wappalyzer(object):
             categorised_apps[app_name] = {"categories": cat_names}
 
         return categorised_apps
+
+
+        
+def _sort_app_versions(a, b):
+    return len(a) - len(b)
