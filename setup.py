3ce3139b0391f9de71c6b4474cc507da4a57fafb
Yuri Kroz <splanger@gmail.com>
2016-11-27 11:14:31 +0200

    Added Python 3 support. Change dependency to BeautifulSoap 4 instead of 3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-Wappalyzer",
-    version="0.2.2",
+    version="0.3.0",
     description="Python implementation of the Wappalyzer web application "
                 "detection utility",
     author="Clay McClure",
@@ -13,12 +13,13 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
     ],
     packages=find_packages(),
     package_data={'Wappalyzer': ['data/apps.json']},
     install_requires=[
-        'BeautifulSoup==3.2.1',
+        'beautifulsoup4==4.5.1',
         'requests',
     ],
     test_suite='nose.collector',
