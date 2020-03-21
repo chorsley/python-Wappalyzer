5beb872b050e64fc73a40ab98a978f0efd6a19c3
byt3bl33d3r <byt3bl33d3r@protonmail.com>
2020-03-21 16:44:04 -0300

    Ported to Python 3, added async support.
    - Now only supports Python 3.7+
    - Added `Webpage.new_from_url_async()` for async support (uses aiohttp)
    - Updated to support changes in new apps.json Wappalyzer file
    - BeautifulSoup now uses lxml as the backend parser
    - Updated dependencies
    - Added MakeFile
    - Added Pipefile
    - Added more tests

---
+++ tests/test_wappalyzer.py
@@ -0,0 +1,88 @@
+import pytest
+import asyncio
+from httpretty import HTTPretty, httprettified
+from Wappalyzer import WebPage, Wappalyzer
+from aioresponses import aioresponses
+
+@pytest.fixture
+def async_mock():
+    with aioresponses() as m:
+        yield m
+
+@httprettified
+def test_new_from_url():
+    HTTPretty.register_uri(HTTPretty.GET, 'http://example.com/',
+                            body='snerble')
+
+    webpage = WebPage.new_from_url('http://example.com/')
+
+    assert webpage.html == 'snerble'
+
+@pytest.mark.asyncio
+async def test_new_from_url_async(async_mock):
+    async_mock.get('http://example.com', status=200, body='snerble')
+
+    webpage = await WebPage.new_from_url_async('http://example.com/')
+
+    assert webpage.html == 'snerble'
+
+def test_latest():
+    analyzer = Wappalyzer.latest()
+
+    print((analyzer.categories))
+    assert analyzer.categories['1']['name'] == 'CMS'
+    assert 'Apache' in analyzer.apps
+
+def test_analyze_no_apps():
+    analyzer = Wappalyzer(categories={}, apps={})
+    webpage = WebPage('http://example.com', '<html></html>', {})
+
+    detected_apps = analyzer.analyze(webpage)
+
+    assert detected_apps == set()
+
+def test_get_implied_apps():
+    analyzer = Wappalyzer(categories={}, apps={
+        'a': {
+            'implies': 'b',
+        },
+        'b': {
+            'implies': 'c',
+        },
+        'c': {
+            'implies': 'a',
+        },
+    })
+
+    implied_apps = analyzer._get_implied_apps('a')
+
+    assert implied_apps == set(['a', 'b', 'c'])
+
+def test_get_analyze_with_categories():
+    webpage = WebPage('http://example.com', '<html>aaa</html>', {})
+    categories = {
+        "1": {
+            "name": "cat1",
+            "priority": 1
+        },
+        "2": {
+            "name": "cat2",
+            "priority": 1
+        }
+    }
+
+    apps = {
+        'a': {
+            'html': 'aaa',
+            'cats': [1],
+        },
+        'b': {
+            'html': 'bbb',
+            'cats': [1, 2],
+        }
+    }
+
+    analyzer = Wappalyzer(categories=categories, apps=apps)
+    result = analyzer.analyze_with_categories(webpage)
+
+    assert result == {"a": {"categories": ["cat1"]}}
