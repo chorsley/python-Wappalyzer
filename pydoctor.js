e4f2b37bd1153e5af7ffe0f69a228fb3617a3eef
tristanlatr <tristanlatr@users.noreply.github.com>
2021-05-27 18:31:33 +0000

    Generate pydoctor documentation ceb2f8ecd88a003bb3f4c9100959cdc0970dc270

--- pydoctor.js
+++ pydoctor.js
@@ -1,13 +1,29 @@
+function initPrivate() {
+    var params = (new URL(document.location)).searchParams;
+    if (!params || !parseInt(params.get('private'))) {
+        var show = false;
+        var hash = document.location.hash;
+        if (hash != '') {
+            var anchor = document.querySelector('a[name="' + hash.substring(1) + '"]');
+            show = anchor && anchor.parentNode.classList.contains('private');
+        }
+        if (!show) {
+            document.body.classList.add("private-hidden");
+        }
+    }
+    updatePrivate();
+}
 function togglePrivate() {
-    // Hide all private things by adding the private-hidden class to them.
     document.body.classList.toggle("private-hidden");
-
-    var button = document.querySelector('#showPrivate button');
-    if(document.body.classList.contains('private-hidden')) {
-        button.innerText = 'Show Private API';
-    } else {
-        button.innerText = 'Hide Private API';
+    updatePrivate();
+}
+function updatePrivate() {
+    var hidden = document.body.classList.contains('private-hidden');
+    document.querySelector('#showPrivate button').innerText =
+        hidden ? 'Show Private API' : 'Hide Private API';
+    if (history) {
+        var search = hidden ? document.location.pathname : '?private=1';
+        history.replaceState(null, '', search + document.location.hash);
     }
 }
-// On load, hide everything private
-togglePrivate()
+initPrivate();
