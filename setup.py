86bb713de2271eeb17e108e23778a42656d0c759
tristanlatr <tris.la.tr@gmail.com>
2022-03-08 23:36:00 -0500

    Do not try to remove default requirements from setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
 from setuptools import setup, find_packages
 import pathlib
-import sys
-
-requirements = [
-        'beautifulsoup4',
-        'lxml',
-        'requests',
-        'aiohttp',
-        'httpretty',
-        'aioresponses',
-        'cached_property',
-    ]
-
-# Simulate that 'beautifulsoup4' and 'lxml' are default extra dependencies
-# that can be disabled by [no-lxml] extra. (https://github.com/pypa/setuptools/issues/1139)
-if any('no-lxml' in arg for arg in sys.argv[1:]):
-    requirements.remove('beautifulsoup4')
-    requirements.remove('lxml')
 
 setup(
     name                =   "python-Wappalyzer",
@@ -28,19 +11,26 @@ setup(
     author              =   "Chris Horsley (chorsley) and other contributors (See git history)",
     url                 =   "https://github.com/chorsley/python-Wappalyzer",
     classifiers         =   [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
+                                'Development Status :: 3 - Alpha',
+                                'Intended Audience :: Developers',
+                                'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                                'Programming Language :: Python :: 3',
+                                'Topic :: Internet :: WWW/HTTP',
+                            ],
     packages            =   find_packages(exclude='tests'),
     package_data        =   {'Wappalyzer': ['data/technologies.json']},
-    install_requires    =   requirements,
-    extras_require      =   {'no-lxml': ['dom-query'],
+    install_requires    =   [
+                                'beautifulsoup4',
+                                'lxml',
+                                'requests',
+                                'aiohttp',
+                                'aioresponses',
+                                'cached_property',
+                            ],
+    extras_require      =   {'no-lxml': ['dom_query'],
                              # Pin pydoctor version until https://github.com/twisted/pydoctor/issues/513 if fixed
                              'docs': ["pydoctor==21.2.2", "docutils"], 
-                             'dev': ["tox", "mypy>=0.902", "pytest", "pytest-asyncio", 
+                             'dev': ["tox", "mypy>=0.902", "httpretty", "pytest", "pytest-asyncio", 
                                      "types-requests", "types-pkg_resources" ]},
     python_requires     =   '>=3.6',
 )
